9b30e41073da3aeb965c88ea216cdfa91c2f1606
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-10-21 10:45:38 +0100

    Adicionar exercícios resolvidos da aula 05

---
+++ Aula05/Exercicio1/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Tooltip("I'm some value!!!!")]
+    [Range(0, 500)]
+    [SerializeField]
+    private int someValue;
+}
